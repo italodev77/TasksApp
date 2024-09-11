e5f32fe62b21a3c4f5916ad46ba5126654857b7b
devitalo <italo_ariel_dev77@outlook.com>
2024-09-11 20:31:08 -0300

    feat: entidades User e workspace criadas

---
+++ Domain/Entity/Workspace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Workspace
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public User? User { get; set; }
+        public ICollection<ListCard>? ListsCards { get; set; }
+        public DateTime? CreatedAt { get; set; }  = DateTime.Now;   
+
+    }
+}
