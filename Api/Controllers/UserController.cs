e94e58ed81af24e801ba281b2e249975062fa620
devitalo <italo_ariel_dev77@outlook.com>
2024-09-18 02:14:02 -0300

    feat: criado o controller para iniciar as operações relacionadas a entidade user

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.UserCQ.Commands;
+using Application.UserCQ.ViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Api.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class UserController(IMediator mediator) : ControllerBase
     {
+        private readonly IMediator _mediator = mediator;
+        public async Task<ActionResult<UserInfoViewModel>> CreateUser( CreateUserCommand comand)
+        {
+            var result = await _mediator.Send(comand);
+            return Ok(result);
+        }
     }
 }
