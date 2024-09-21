d6b72c6d6b7ced2e248572b9235489195760870a
devitalo <italo_ariel_dev77@outlook.com>
2024-09-21 20:58:22 -0300

    feat: criação da classe BuilderExtensions para melhorar o codigo e leitura da classe program.

---
+++ Api/BuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Application.UserCQ.Commands;
+using Application.UserCQ.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Infra.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api
+{
+    public static class BuilderExtensions
+    {
+        public static void AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(CreateUserCommand).Assembly));
+
+        }
+        public static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            var configuration = builder.Configuration;
+            builder.Services.AddDbContext<TasksDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        }
+        public static void AddValidations(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddValidatorsFromAssemblyContaining<CreateUserComandValidator>();
+            builder.Services.AddFluentValidationAutoValidation();
+
+
+        }
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,28 +1,14 @@
-using Infra.Persistence;
-using Microsoft.EntityFrameworkCore;
-using MediatR;
-using Application.UserCQ.Commands;
-using FluentValidation;
-using Application.UserCQ.Validators;
-using FluentValidation.AspNetCore;
+using Api;
 
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TasksDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddMediatR( config => config.RegisterServicesFromAssemblies(typeof (CreateUserCommand).Assembly));
-builder.Services.AddValidatorsFromAssemblyContaining<CreateUserComandValidator>();
-builder.Services.AddFluentValidationAutoValidation();
+builder.AddServices();
+builder.AddDatabase();
+builder.AddValidations();   
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
